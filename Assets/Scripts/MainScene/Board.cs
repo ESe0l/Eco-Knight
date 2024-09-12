82cde7006b716a2474c3d1f270deaf222f6da0ce
Eunseol Park <kongpark2019@gmail.com>
2024-09-12 14:11:09 +0900

    카드 배치

--- Assets/Scripts/MainScene/Board.cs
+++ Assets/Scripts/MainScene/Board.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
+    public Transform cards;
+    public GameObject card;
     // Start is called before the first frame update
     void Start()
     {
-        
+        int[] arr = {0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 };
+        for (int i = 0; i <8; i++) //9, 16, 25, ? //0~7
+        {
+            GameObject go = Instantiate(card);
+            go.transform.parent = cards;
+
+            float x = (i % 4) * 1.4f - 1.89f;
+            float y = (i / 4) * 1.4f - 0.19f;
+            go.transform.position = new Vector2(x, y);
+        }
     }
 
     // Update is called once per frame
