adbc2f823c35d5b03cacef2b88044e4a40cb0a9a
Eunseol Park <kongpark2019@gmail.com>
2024-09-12 16:14:30 +0900

    카드 및 보드

--- Assets/Scripts/MainScene/Board.cs
+++ Assets/Scripts/MainScene/Board.cs
@@ -1,23 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
 
 public class Board : MonoBehaviour
 {
     public Transform cards;
     public GameObject card;
+    
     // Start is called before the first frame update
     void Start()
     {
-        int[] arr = {0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 };
-        for (int i = 0; i <8; i++) //9, 16, 25, ? //0~7
-        {
-            GameObject go = Instantiate(card);
-            go.transform.parent = cards;
+	    int[] arr = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6 ,7, 7};
+		arr = arr.OrderBy(x => UnityEngine.Random.Range(0f, 7f)).ToArray();
 
-            float x = (i % 4) * 1.4f - 1.89f;
-            float y = (i / 4) * 1.4f - 0.19f;
+        for (int i = 0; i < 16; i++)
+        {
+            GameObject go = Instantiate(card, this.transform);
+
+            float x = (i / 4) * 1.4f - 1.89f;
+            float y = (i % 4) * 1.4f - 0.29f;
             go.transform.position = new Vector2(x, y);
+
+            go.GetComponent<Card>().Setting(arr[i]);
         }
     }
 
--- Assets/Scripts/MainScene/GameManger.cs
+++ Assets/Scripts/MainScene/GameManger.cs
@@ -5,8 +5,11 @@ using UnityEngine.UI;
 
 public class GameManger : MonoBehaviour
 {
+    public static GameManger Instance;
     public Text TimeTxt;
     float time = 60.0f;
+    public int cardCount = 0;
+    int level = 0;
 
     // Start is called before the first frame update
     void Start()
