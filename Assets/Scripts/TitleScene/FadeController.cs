1c17b7c305e803cd6a59c30ed9267d243dbf8455
jsj2518 <joseph2518@naver.com>
2024-09-12 12:17:04 +0900

    타이틀 기능 추가
    시작버튼 추가(기능 포함)
    배경화면 추가
    옵션버튼,, 효과 판넬 추가(기능구현 X)

---
+++ Assets/Scripts/TitleScene/FadeController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeController : MonoBehaviour
+{
+    public bool isFadeIn;
+
+    public CanvasGroup canvasGroup;
+
+    float elapsedTime = 0f;
+    float fadedTime = 1f;
+}
