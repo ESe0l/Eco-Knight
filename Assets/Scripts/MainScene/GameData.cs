4c9ca06409b0ea88fc01983e77da3df1409df6d6
jsj2518 <joseph2518@naver.com>
2024-09-12 22:48:41 +0900

    게임 데이터에 레코드 추가

--- Assets/Scripts/MainScene/GameData.cs
+++ Assets/Scripts/MainScene/GameData.cs
@@ -10,12 +10,19 @@ public class GameData : MonoBehaviour
     public int cardSet = 8;
     public float timeSet = 60.0f;
     public bool actGimmick = false;
+
+    public float[] record = new float[10]; //Best Record  (index <- stageNum)
+
     private void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
         }
     }
 }
